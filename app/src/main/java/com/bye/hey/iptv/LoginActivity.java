81f9c7563f7a20a87265cf42792830e16550af6a
Ayush Gupta <ayushpguptaapg@gmail.com>
2017-07-25 01:00:12 +0530

    Initial commit

---
+++ app/src/main/java/com/bye/hey/iptv/LoginActivity.java
@@ -0,0 +1,95 @@
+package com.bye.hey.iptv;
+
+import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_main);
+
+        final EditText username = findViewById(R.id.username);
+        final EditText password = findViewById(R.id.password);
+
+        findViewById(R.id.login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (username.getText().toString().trim().length() > 0) {
+                    if (password.getText().toString().trim().length() > 0) {
+                        startiptvapp(username.getText().toString(), password.getText().toString());
+                    } else
+                        Toast.makeText(LoginActivity.this, "Password cannot be empty!", Toast.LENGTH_SHORT).show();
+                } else
+                    Toast.makeText(LoginActivity.this, "Username cannot be empty!", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
+
+    private void startiptvapp(String username, String password) {
+        try {
+            Intent intent = new Intent();
+            intent.setClassName(_IPTV_CORE_PACKAGE_NAME, _IPTV_CORE_CLASS_NAME);
+
+            // Set your playlist url and uncomment the lines below
+            String playlistUrl = "http://diogen.online:25461/get.php?username=" + username + "&password=" + password + "&type=m3u&output=ts";
+            intent.setData(Uri.parse(playlistUrl));
+
+            // If "package" extra is set, IPTV Core will be able to show your app name as a title
+            intent.putExtra("package", getPackageName());
+
+            // EPG URL can be set either by "url-tvg" parameter in your playlist or by the following extra (supported since IPTV Core 3.3)
+            // intent.putExtra("url-tvg", "<EPG URL>");
+
+            startActivity(intent);
+            finish();
+        } catch (ActivityNotFoundException e) {
+            // IPTV core app is not installed, let's ask the user to install it.
+            showIptvCoreNotFoundDialog();
+        }
+    }
+
+    public void showIptvCoreNotFoundDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.dialog_core_not_installed_title);
+        builder.setMessage(R.string.dialog_core_not_installed_message);
+        builder.setPositiveButton(R.string.dialog_button_install,
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialogInterface, int id) {
+                        try {
+                            // try to open Google Play app first
+                            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + _IPTV_CORE_PACKAGE_NAME)));
+                        } catch (ActivityNotFoundException e) {
+                            // if Google Play is not found for some reason, let's open browser
+                            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com/store/apps/details?id=" + _IPTV_CORE_PACKAGE_NAME)));
+                        }
+                    }
+                });
+        builder.setNegativeButton(R.string.dialog_button_cancel,
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialogInterface, int id) {
+                        // if cancelled - just close the app
+                        finish();
+                    }
+                });
+        builder.setCancelable(false);
+        builder.create().show();
+    }
+
+
+    private static final String _IPTV_CORE_PACKAGE_NAME = "ru.iptvremote.android.iptv.core";
+    private static final String _IPTV_CORE_CLASS_NAME = _IPTV_CORE_PACKAGE_NAME + ".ChannelsActivity";
+}
+
+
---
+++ app/src/main/java/com/bye/hey/iptv/MainActivityiptv.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (C) 2014 Alexander Sofronov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bye.hey.iptv;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+
+public class MainActivityiptv
+    extends Activity
+{
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        
+        try {
+            Intent intent = new Intent();
+            intent.setClassName(_IPTV_CORE_PACKAGE_NAME, _IPTV_CORE_CLASS_NAME);
+
+            // Set your playlist url and uncomment the lines below   
+            // String playlistUrl = "<PLAYLIST URL>";
+            // intent.setData(Uri.parse(playlistUrl));
+            
+            // If "package" extra is set, IPTV Core will be able to show your app name as a title
+            intent.putExtra("package", getPackageName());
+            
+            // EPG URL can be set either by "url-tvg" parameter in your playlist or by the following extra (supported since IPTV Core 3.3)
+            // intent.putExtra("url-tvg", "<EPG URL>");
+
+            startActivity(intent);
+            finish();
+        } catch (ActivityNotFoundException e) {
+            // IPTV core app is not installed, let's ask the user to install it.
+            showIptvCoreNotFoundDialog();
+        }
+    }
+    
+    public void showIptvCoreNotFoundDialog()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.dialog_core_not_installed_title);
+        builder.setMessage(R.string.dialog_core_not_installed_message);
+        builder.setPositiveButton(R.string.dialog_button_install,
+            new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialogInterface, int id)
+                {
+                    try {
+                        // try to open Google Play app first 
+                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + _IPTV_CORE_PACKAGE_NAME)));
+                    } catch (ActivityNotFoundException e) {
+                        // if Google Play is not found for some reason, let's open browser
+                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com/store/apps/details?id=" + _IPTV_CORE_PACKAGE_NAME)));
+                    }
+                }
+            });
+        builder.setNegativeButton(R.string.dialog_button_cancel,
+            new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialogInterface, int id)
+                {
+                    // if cancelled - just close the app
+                    finish();
+                }
+            });
+        builder.setCancelable(false);
+        builder.create().show();
+    }
+
+    
+    private static final String _IPTV_CORE_PACKAGE_NAME = "ru.iptvremote.android.iptv.core";
+    private static final String _IPTV_CORE_CLASS_NAME = _IPTV_CORE_PACKAGE_NAME + ".ChannelsActivity";
+}
